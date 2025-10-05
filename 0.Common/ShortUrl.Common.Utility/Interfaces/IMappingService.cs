1fbf16db16f6eb795635522a1c2ac292de7ac973
Samira Khaleghi <samirakhaleghideveloper@gmail.com>
2025-10-05 10:39:39 +0330

    AddIMappingService

---
+++ 0.Common/ShortUrl.Common.Utility/Interfaces/IMappingService.cs
@@ -0,0 +1,10 @@
+﻿namespace ShortUrl.Common.Utility.Interfaces
+{
+    public interface IMappingService
+    {
+        TDestination Map<TDestination>(object source);
+        TDestination Map<TSource, TDestination>(TSource source);
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+        IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source);
+    }
+}
